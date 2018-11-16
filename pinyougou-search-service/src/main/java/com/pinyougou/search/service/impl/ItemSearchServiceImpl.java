45885577fd969d846ec0107de5ae5abef4083742
noobHaa <lilei642318580@163.com>
2018-11-16 17:45:48 +0800

    完成高亮

--- pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -5,12 +5,13 @@ import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.Query;
-import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,10 +25,29 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 
     @Override
     public Map<String, Object> search(Map searchMap) {
-        Query query = new SimpleQuery("*:*");
+       /* Query query = new SimpleQuery("*:*");
         Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
         query.addCriteria(criteria);
-        ScoredPage<TbItem> items = solrTemplate.queryForPage(query, TbItem.class);
+        ScoredPage<TbItem> items = solrTemplate.queryForPage(query, TbItem.class);*/
+        //设置高亮
+        HighlightQuery highlightQuery = new SimpleHighlightQuery();
+        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+        highlightQuery.addCriteria(criteria);
+        HighlightOptions highlightOptions = new HighlightOptions();
+        //设置高亮的字段和样式
+        highlightOptions.addField("item_title").setSimplePrefix("<em style='color:red'>").setSimplePostfix("</em>");
+
+        highlightQuery.setHighlightOptions(highlightOptions);
+        HighlightPage<TbItem> items = solrTemplate.queryForHighlightPage(highlightQuery, TbItem.class);
+
+        List<HighlightEntry<TbItem>> highlighted = items.getHighlighted();
+        for (HighlightEntry<TbItem> h : highlighted) {
+            TbItem item = h.getEntity();
+            if (h.getHighlights().size() > 0 && h.getHighlights().get(0).getSnipplets().size() > 0) {
+                item.setTitle(h.getHighlights().get(0).getSnipplets().get(0));
+            }
+        }
+
         Map<String, Object> map = new HashMap<>();
         map.put("rows", items.getContent());
         return map;
